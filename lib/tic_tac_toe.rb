60acfaf5a4a4d504eef92c28c3ec892af0a9bc47
Joyce Chun <wooyangee@gmail.com>
2018-04-12 04:25:55 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -18,7 +18,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(board, input)
+def input_to_index(board, index)
 index = input.to_i - 1
 board[input] = index
 end
