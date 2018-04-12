ef844eea9b891526afe952c8ae1855750e0e1a8a
Joyce Chun <wooyangee@gmail.com>
2018-04-12 05:50:40 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -48,7 +48,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index, "X")
+    move(board, index, "X" || "O")
     display_board(board)
   else
     turn(board)
