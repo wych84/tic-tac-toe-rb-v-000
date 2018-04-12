b5d8c7569e9c213e96d445b249f4db533f54f0c5
Joyce Chun <wooyangee@gmail.com>
2018-04-12 04:50:40 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -48,7 +48,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index, character)
+    move(board, index)
     display_board(board)
   else
     puts "Not valid."
