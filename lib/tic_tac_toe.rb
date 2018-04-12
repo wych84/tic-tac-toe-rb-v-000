a0ce3975c15d3b219871d4767de526ff283b8ef5
Joyce Chun <wooyangee@gmail.com>
2018-04-12 05:33:13 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -74,7 +74,36 @@ def current_player(board)
   end
 end
 
+def won?(board)
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
+  end
+end
 
+
+def full?(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+  if board.any? {|z| z == " "}
+    false 
+  else
+    true
+  end
+end
+
+def draw?(board)
+  full?(board) && !(won?(board))
+end
+
+def over?(board)
+  full?(board) || draw?(board) || won?(board)
+end
+
+def winner(board)
+  if winning_combo = won?(board)
+    board[winning_combo.first]
+  end
+end  
 
   
   
