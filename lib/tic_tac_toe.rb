f19983e1865d65e67223cff55b886ac4a737d9dd
Joyce Chun <wooyangee@gmail.com>
2018-04-12 04:21:09 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,13 +1,3 @@
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
 WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
@@ -18,3 +8,15 @@ WIN_COMBINATIONS = [
   [0,4,8],
   [2,4,6]]
 
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+input_to_index(input, index)
+board[input] = 
