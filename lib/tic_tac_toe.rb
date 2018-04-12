51bef992d58ab7eb20d57a4586825bb3c27c7418
Joyce Chun <wooyangee@gmail.com>
2018-04-12 04:48:52 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -46,7 +46,7 @@ end
 def turn(board)
   puts "Fill your spot in spaces 1-9:"
   input = gets.strip
-  input_to_index(input)
+  index = input_to_index(input)
   if valid_move?(board, index)
     display_board(board)
   else
